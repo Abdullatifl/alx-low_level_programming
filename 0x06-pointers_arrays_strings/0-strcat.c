6ff885cd1f6f7c3b9cd35acef910be962fee770d
Abdullatifl <1abdullatif.ah@gmail.com>
2023-09-20 14:56:52 -0700

    0. strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,22 +8,22 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int z;
+	int i;
 	int j;
 
-	z = 0;
-	while (dest[z] != '\0')
+	i = 0;
+	while (dest[i] != '\0')
 	{
 		i++;
 	}
 	j = 0;
 	while (src[j] != '\0')
 	{
-		dest[z] = src[j];
+		dest[i] = src[j];
 		i++;
 		j++;
 	}
 
-	dest[z] = '\0';
+	dest[i] = '\0';
 	return (dest);
 }
